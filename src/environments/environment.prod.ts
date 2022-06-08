a421962186450d5e6f4bbaa680a9610370004e2b
Alexander De Azevedo <alexander_deazevedo@hotmail.com>
2022-06-08 10:50:00 -0400

    Actualizar enlaces enviroment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   contentType: "application/json",
-  url: "https://sistema-de-videovigilancia.herokuapp.com:8000/api/",
-  shortURL: "https://sistema-de-videovigilancia.herokuapp.com/:8000",
+  url: "https://sistema-de-videovigilancia.herokuapp.com/api/",
+  shortURL: "https://sistema-de-videovigilancia.herokuapp.com",
   peerjsHost: "https://sistema-de-videovigilancia.herokuapp.com",
   peerJSPort: 3001
 };
