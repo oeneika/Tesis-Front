06203e836a12a24c44df3c24b3b0aa7f7c813206
Alexander De Azevedo <alexander_deazevedo@hotmail.com>
2022-10-05 17:46:55 -0400

    Cambiando el env de prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   contentType: "application/json",
   url: "https://sistema-de-videovigilancia.herokuapp.com/api/",
-  shortURL: "https://sistema-de-videovigilancia.herokuapp.com",
-  peerjsHost: "sistema-de-videovigilancia.herokuapp.com",
+  shortURL: "https://sistema-de-videovigilancia.herokuapp.com/",
+  peerjsHost: "ws://sistema-de-videovigilancia.herokuapp.com",
   peerJSPort: 3001
 };
